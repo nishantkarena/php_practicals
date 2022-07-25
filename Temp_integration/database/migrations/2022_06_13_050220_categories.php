0e893fb541aaec252df3d3557b11e209575c1cf7
NISHANT KARENA <nishant.karena@kcsitglobal.com>
2022-07-25 17:50:48 +0530

    Template Integration

--- Temp_integration/database/migrations/2022_06_13_050220_categories.php
+++ Temp_integration/database/migrations/2022_06_13_050220_categories.php
@@ -15,7 +15,7 @@ class Categories extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('cname');
             $table->enum('active',['1','0']);
             $table->softDeletes();
         });
